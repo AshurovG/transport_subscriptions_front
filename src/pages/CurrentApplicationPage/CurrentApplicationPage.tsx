5b2c521df3ed6f1c420c89d485ecd41d7d652e9a
Georgy <ashurovgeorgy@MacBook-Pro-som-tillhor-Georgy.local>
2023-11-25 20:25:24 +0300

    add delete application request

--- src/pages/CurrentApplicationPage/CurrentApplicationPage.tsx
+++ src/pages/CurrentApplicationPage/CurrentApplicationPage.tsx
@@ -42,8 +42,29 @@ const CurrentApplicationPage = () => {
     }
   }
 
+  const deleteApplication = async () => {
+    try {
+      const response = await axios(`http://localhost:8000/applications/delete`, {
+      method: 'DELETE',
+      withCredentials: true
+    })
+
+    dispatch(setSubscriptionsFromApplicationAction([]));
+    dispatch(setCurrentApplicationDateAction(''));
+    toast.success("Заявка успешно удалена!");
+    }
+    catch(error) {
+      throw error;
+    }
+    
+  }
+
   const handleSendButtonClick = () => {
     sendApplication();
+  }
+
+  const handleDeleteButtonClick = () => {
+    deleteApplication();
   }
 
   return (
@@ -54,19 +75,25 @@ const CurrentApplicationPage = () => {
           Текущая заявка
         </h1>
 
-        <h5 className={styles['application__page-subtitle']}>
-          У вас есть возможность удалять абонементы из заявки, удалить всю заявку или отправить заявку на проверку модераторам!
-        </h5>
+        {subscriptions.length !== 0 ? <div>
+          <h5 className={styles['application__page-subtitle']}>
+            У вас есть возможность удалять абонементы из заявки, удалить всю заявку или отправить заявку на проверку модераторам!
+          </h5>
 
-        <div className={styles['application__page-info']}>
-          <h3 className={styles['application__page-info-title']}>Дата создания заявки: <b>{applicationDate}</b></h3>
-          <SubscriptionsTable subscriptions={subscriptions} className={styles['application__page-info-table']}/>
+          <div className={styles['application__page-info']}>
+            <h3 className={styles['application__page-info-title']}>Дата создания заявки: <b>{applicationDate}</b></h3>
+            <SubscriptionsTable subscriptions={subscriptions} className={styles['application__page-info-table']}/>
 
-          <div className={styles['application__page-info-btns']}>
-            <Button onClick={() => handleSendButtonClick()} className={styles['application__page-info-btn']}>Отправить</Button>
-            <Button className={styles['application__page-info-btn']}>Удалить</Button>
+            <div className={styles['application__page-info-btns']}>
+              <Button onClick={() => handleSendButtonClick()} className={styles['application__page-info-btn']}>Отправить</Button>
+              <Button onClick={() => handleDeleteButtonClick()} className={styles['application__page-info-btn']}>Удалить</Button>
+            </div>
           </div>
         </div>
+        : <h5 className={styles['application__page-subtitle']}>
+            На данный момент ваша заявка пустая! Вы можете добавить различные абонементы в заявку, и отправить ее на поверку модераторам!
+          </h5>
+      }
       </div>
     </div>
   )
