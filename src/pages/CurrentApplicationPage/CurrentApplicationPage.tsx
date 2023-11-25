d30a182f68eaa1dbf8cf60f2194610ef1443e915
Georgy <ashurovgeorgy@MacBook-Pro-som-tillhor-Georgy.local>
2023-11-25 20:15:12 +0300

    add send application request

--- src/pages/CurrentApplicationPage/CurrentApplicationPage.tsx
+++ src/pages/CurrentApplicationPage/CurrentApplicationPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import axios from 'axios'
+import { toast } from 'react-toastify';
 import styles from './CurrentApplicationPage.module.scss'
 import Header from 'components/Header'
 import Button from 'react-bootstrap/Button'
 import { useCurrentApplicationId } from 'Slices/ApplicationsSlice'
 import SubscriptionsTable from 'components/SubscriptionsTable'
-import { mockSubscriptions } from '../../../consts';
 import { useDispatch } from 'react-redux'
 import { useCurrentApplicationDate, useSubscripitonsFromApplication,
   setCurrentApplicationDateAction, setSubscriptionsFromApplicationAction, setCurrentApplicationIdAction } from 'Slices/ApplicationsSlice'
@@ -25,7 +25,26 @@ const CurrentApplicationPage = () => {
   const dispatch = useDispatch();
   const subscriptions = useSubscripitonsFromApplication();
   const applicationDate = useCurrentApplicationDate();
-  const currentApplicationId = useCurrentApplicationId()
+  const currentApplicationId = useCurrentApplicationId();
+
+  const sendApplication = async () => {
+    try {
+      const response = await axios(`http://localhost:8000/applications/send`, {
+        method: 'PUT',
+        withCredentials: true
+      })
+
+      dispatch(setSubscriptionsFromApplicationAction([]));
+      dispatch(setCurrentApplicationDateAction(''));
+      toast.success("Заявка успешно отправлена на проверку!");
+    } catch(error) {
+      throw error;
+    }
+  }
+
+  const handleSendButtonClick = () => {
+    sendApplication();
+  }
 
   return (
     <div className={styles.application__page}>
@@ -44,7 +63,7 @@ const CurrentApplicationPage = () => {
           <SubscriptionsTable subscriptions={subscriptions} className={styles['application__page-info-table']}/>
 
           <div className={styles['application__page-info-btns']}>
-            <Button className={styles['application__page-info-btn']}>Отправить</Button>
+            <Button onClick={() => handleSendButtonClick()} className={styles['application__page-info-btn']}>Отправить</Button>
             <Button className={styles['application__page-info-btn']}>Удалить</Button>
           </div>
         </div>
