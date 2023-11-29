e74323305ed141632cf37c1ee5a1638504ff8301
Georgy <ashurovgeorgy@MacBook-Pro-som-tillhor-Georgy.local>
2023-11-29 22:56:55 +0300

    add adaptive for applications page and current application page

--- src/pages/CurrentApplicationPage/CurrentApplicationPage.tsx
+++ src/pages/CurrentApplicationPage/CurrentApplicationPage.tsx
@@ -91,7 +91,7 @@ const CurrentApplicationPage = () => {
           </h5>
 
           <div className={styles['application__page-info']}>
-            <h3 className={styles['application__page-info-title']}>Дата создания заявки: <b>{applicationDate}</b></h3>
+            <h3 className={styles['application__page-info-title']}>Дата создания заявки: <br/><b>{applicationDate}</b></h3>
             <h3 className={styles['application__page-info-title']}>Добавленные абонементы:</h3>
             <SubscriptionsTable subscriptions={subscriptions} className={styles['application__page-info-table']}/>
 
